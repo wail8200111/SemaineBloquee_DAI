2d025f971a25c3df871ee23a273ef6829f0166d7
Wail <wail82000@gmail.com>
2023-02-04 17:10:56 +0100

    commit bd

--- src/main/java/BD/bd.java
+++ src/main/java/BD/bd.java
@@ -1,5 +1,60 @@
 package BD;
 
-public class bd {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
-}
+public class bd {
+	
+	/*---------*/
+	/* Données */
+	/*---------*/
+
+	/*----- Connexion -----*/
+	private static Connection cx = null;
+
+	/*----- Données de connexion -----*/
+	private static final String URL			= "jdbc:mysql://localhost:3307/db_22005089";
+	private static final String LOGIN		= "22005089";
+	private static final String PASSWORD	= "4616EH";
+
+
+	/*----------*/
+	/* Méthodes */
+	/*----------*/
+
+	/**
+	 * Crée la connexion avec la base de données.
+	 */
+	private static void connexion() throws ClassNotFoundException, SQLException {
+		/*----- Chargement du pilote pour la BD -----*/
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			}
+		catch (ClassNotFoundException ex)
+			{
+			throw new ClassNotFoundException("Exception Bd.connexion() : Pilote MySql introuvable - " + ex.getMessage());
+			}
+
+		/*----- Ouverture de la connexion -----*/
+		try {
+			bd.cx = DriverManager.getConnection(URL,LOGIN,PASSWORD);
+			}
+		catch (SQLException ex)
+			{
+			throw new SQLException("Exception connexion() : Problème de connexion à la base de données - " + ex.getMessage());
+			}
+	}
+	
+	/*----------------------------*/
+	/* Programme principal (test) */
+	/*----------------------------*/
+
+	public static void main (String[] s){
+		
+	}
+
+}	
+
+ /*----- Fin de la classe Bd -----*/
